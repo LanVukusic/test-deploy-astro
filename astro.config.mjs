4ed1b81562589ad844e83e9f28dd52202605f6df
Lan Vukušič <lan.vukusic@gmail.com>
2022-08-22 00:46:59 +0200

    website overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [
+    tailwind({
+      config: {
+        path: "./tailwind.config.cjs",
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+  ],
 });
